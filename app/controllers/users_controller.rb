2a17e2d9dc59c426c8e53fbb29e9724b66a0dc47
Tunisia Artope <artope@yahoo.com>
2022-04-11 14:16:09 -0400

    commit tree 9bc6653a1695a32d7dfba1a1976e3325d72c54d4

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,28 @@
+class UsersController < ApplicationController
+    skip_before_action :authorize, only: :create
+
+    def create 
+        user = User.create!(user_params)
+        session[:user_id] = user.id
+        render json: user, status: :created
+    end
+
+    def show
+        user = User.find_by(id: session[:user_id])
+        render json: user, status: :created
+    end
+
+    private
+
+    def user_params
+        params.permit(:username, :password, :password_confirmation, :image_url, :bio)
+    end
+
+    # def authorize
+    #     return render json: { error: "Not authorized"}, status: :unauthorized unless session.include? :user_id
+    # end
+
+    # def render_unprocessable_entity(invalid)
+    #     render json: { error: invalid.record.errors }, status: :unprocessable_entity
+    # end
+end
